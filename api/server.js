dcfc73508476c6b28e9364198373d71fcae7138a
deshan-knightowl <deshan@knightowl.online>
2025-06-26 16:29:13 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -5,7 +5,7 @@ import { Pinecone } from "@pinecone-database/pinecone";
 import dotenv from "dotenv";
 import { randomUUID } from "crypto";
 
-dotenv.config();
+dotenv.config(); 
 
 const app = express();
 const { json } = pkg;
